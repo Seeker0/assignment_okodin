520900501e3c34a95262896ee1b74e3df1370cc0
Austin1780 <austinwilliamsmith@gmail.com>
2017-12-05 11:09:29 -0700

    set up initial handlebars

--- repl.js
+++ repl.js
@@ -1,16 +1,13 @@
-const repl = require('repl').start({});
-const lodash = require('lodash');
-const helpers = require('./helpers');
-
-
-
+const repl = require("repl").start({});
+const lodash = require("lodash");
+const helpers = require("./helpers");
+const models = require("./models");
 
 // ----------------------------------------
 // Libs
 // ----------------------------------------
 repl.context.lodash = lodash;
-
-
+repl.context.models = models;
 
 // ----------------------------------------
 // Helpers
@@ -19,13 +16,22 @@ repl.context.helpers = helpers;
 Object.keys(helpers).forEach(key => {
   repl.context[key] = helpers[key];
 });
-
+Object.keys(models).forEach(modelName => {
+  repl.context[modelName] = models[modelName];
+});
 
 // ----------------------------------------
 // Logging
 // ----------------------------------------
-repl.context.lg = console.log;
-
-
-
-
+repl.context.lg = data => {
+  if (Array.isArray(data)) {
+    if (data.length && data[0].dataValues) {
+      data = data.map(item => item.dataValues);
+    }
+  } else {
+    if (data.dataValues) {
+      data = data.dataValues;
+    }
+  }
+  console.log(data);
+};
